4ba33919a69c0e60f83a2dbdb55e58f8c0e433d1
Ryan Cheevers <65794698+rcheevers@users.noreply.github.com>
2023-04-14 14:38:25 -0500

    change url

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ try {
 
 if (
     process.env.NODE_ENV === `production` &&
-    config.siteUrl === `http://localhost:8000` &&
+    config.siteUrl === `gatsbystarterghost16047.gatsbyjs.io` &&
     !process.env.SITEURL
 ) {
     throw new Error(
